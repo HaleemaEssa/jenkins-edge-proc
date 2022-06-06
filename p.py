6535767017e92e1148c09919d57a7d306ef9339d
HaleemaEssa <70490977+HaleemaEssa@users.noreply.github.com>
2022-06-06 14:26:27 +0300

    Update p.py

--- p.py
+++ p.py
@@ -5,10 +5,10 @@ import time
 import csv
 import pandas
 from csv import reader
-import pandas as pdd
-dfd= pdd.read_csv("/data/data.csv", sep="\t or ,")
-dfd.drop_duplicates(subset=['Date','Sound','Flame','Humidity','Temperature'],keep='first')
-dfd.to_csv("/data/datadrop.csv")
+#import pandas as pdd
+#dfd= pdd.read_csv("/data/data.csv", sep="\t or ,")
+#dfd.drop_duplicates(subset=['Date','Sound','Flame','Humidity','Temperature'],keep='first')
+#dfd.to_csv("/data/datadrop.csv")
 #import pandas as pdd
 #dfd=pdd.read_csv("/data/data.csv")
 #dfd.drop_duplicates(subset=None,inplace=True)
